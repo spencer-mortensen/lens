87c733dd6552b7af6acd21076c6f3eb8d19bf8c9
Spencer Mortensen <spencer@spencermortensen.com>
2018-01-28 12:14:58 -0500

    0.0.52

--- src/Commands/Version.php
+++ src/Commands/Version.php
@@ -33,7 +33,7 @@ class Version implements Command
 	private $arguments;
 
 	/** @var string */
-	const VERSION = '0.0.51';
+	const VERSION = '0.0.52';
 
 	public function __construct(Arguments $arguments)
 	{
