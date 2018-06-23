71e850d485ed8266dd7260ab1279795c09188fd4
Spencer Mortensen <spencer@spencermortensen.com>
2018-06-23 01:46:54 -0400

    Restored the Composer executable

--- lens/autoload.php
+++ lens/autoload.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Lens_0_0_56\SpencerMortensen\Autoloader;
+namespace Lens_0_0_57\SpencerMortensen\Autoloader;
 
 call_user_func(function() {
 	$project = dirname(__DIR__);
 	$namespaces = [
-		'Lens_0_0_56' => 'src'
+		'Lens_0_0_57' => 'src'
 	];
 
 	require_once "{$project}/src/SpencerMortensen/Autoloader/Autoloader.php";
