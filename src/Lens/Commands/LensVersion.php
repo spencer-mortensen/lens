c2089233ef89e357c301f893f3944018a866025b
Spencer Mortensen <spencer@spencermortensen.com>
2018-06-27 14:36:33 -0400

    0.0.59

--- src/Lens/Commands/LensVersion.php
+++ src/Lens/Commands/LensVersion.php
@@ -33,7 +33,7 @@ class LensVersion implements Command
 	private $arguments;
 
 	/** @var string */
-	const VERSION = '0.0.58';
+	const VERSION = '0.0.59';
 
 	public function __construct(Arguments $arguments)
 	{
